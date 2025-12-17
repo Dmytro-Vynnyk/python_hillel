abb2c8be32ff876c78977c88ce8fd0ee1c691b13
Dmytro-Vynnyk <dafsauy8@gmail.com>
2025-12-17 11:52:10 +0200

    Solution via ternary operator

--- DZ_4.1.py
+++ DZ_4.1.py
@@ -3,9 +3,7 @@ no_zero = []
 zero_count = 0
 
 for num in lst:
-    if num == 0:
-        zero_count += 1
-    else:
-        no_zero.append(num)
+    zero_count += 1 if num == 0 else 0
+    no_zero.append(num) if num != 0 else None
 
 print(no_zero + [0] * zero_count)
