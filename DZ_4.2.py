94b8549c309450f7d19e3d2a22f8cdc1ae8ec8d6
Dmytro-Vynnyk <dafsauy8@gmail.com>
2025-12-17 12:05:18 +0200

    Change code

--- DZ_4.2.py
+++ DZ_4.2.py
@@ -1,11 +1,8 @@
 lst = []
 
-if len(lst) == 0:
-    print(0)
-else:
-    sum = 0
+total = 0
+for i in range(0, len(lst), 2):
+    total += lst[i]
 
-    for i in range(1, len(lst), 2):
-        sum += lst[i]
-
-    print(sum * lst[-1])
+result = 0 if not lst else total * lst[-1]
+print(result)
